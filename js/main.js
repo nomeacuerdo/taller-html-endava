e65f4847af93f85162b7b5ba8e5a48a53b729953
Nicolas Arteaga <nicolas.arteaganieto@endava.com>
2017-07-27 17:58:57 -0500

    Created the links handling

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 (function() {
   document.addEventListener('DOMContentLoaded', function(event) {
     var titles = document.querySelectorAll('article.card'),
+        links = document.querySelectorAll('.archive a'),
         activeItem;
 
     Array.prototype.map.call(titles, function(title) {
@@ -10,6 +11,18 @@
         activeItem.classList.remove('active');
 
         this.classList.add('active');
+      });
+    });
+
+    Array.prototype.map.call(links, function(link) {
+      link.addEventListener('click', function() {
+        var title = this.getAttribute('href');
+        title = title.substr(1, title.length);
+        selectedItem = document.getElementById(title);
+        activeItem = document.querySelector('article.active');
+
+        activeItem.classList.remove('active');
+        selectedItem.classList.add('active');
       });
     });
   });
